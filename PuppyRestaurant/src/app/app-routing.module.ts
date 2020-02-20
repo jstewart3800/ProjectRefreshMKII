283bb8401217ee5be5004313364feed960f94e5e
Jon Stewart <56611713+jstewart3800@users.noreply.github.com>
2020-02-19 20:53:30 -0800

    Head, home, footer. Nav toggle broke, need montauk

--- PuppyRestaurant/src/app/app-routing.module.ts
+++ PuppyRestaurant/src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { CateringComponent } from './views/catering/catering.component';
+import { MenuComponent } from './views/menu/menu.component';
+import { PhotosComponent } from './views/photos/photos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'catering', component: CateringComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'photos', component: PhotosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  HomeComponent,
+  AboutComponent,
+  CateringComponent,
+  MenuComponent,
+  PhotosComponent
+]
--- PuppyRestaurant/src/app/app.module.ts
+++ PuppyRestaurant/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { MenuComponent } from './views/menu/menu.component';
+import { CateringComponent } from './views/catering/catering.component';
+import { PhotosComponent } from './views/photos/photos.component';
+import { FooterComponent } from './views/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    MenuComponent,
+    CateringComponent,
+    PhotosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
